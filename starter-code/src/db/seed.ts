e675df07a432f8817bc39938a350e4f4b5df50d7
laurenwelborn <lulu.welborn@gmail.com>
2017-08-21 20:23:11 -0600

    sprint 1

--- starter-code/src/db/seed.ts
+++ starter-code/src/db/seed.ts
@@ -1,14 +1,33 @@
 import { db } from '../models';
 let DB = db.models;
 
+var songCreate = function(artist) {
+	// console.log(artist);
+	return DB.Song.create({
+	    title: 'The Best Song Ever',
+	    duration: '3:31',
+	    date_of_release: '7/13/2015',
+	    album_title: 'Best Album Ever'
+	})
+	.then(function() {
+		// console.log(lucySongs.length);
+  		lucySongs.forEach(function(song) {
+  			song.artistId = artist.id;
+  		});
+  		DB.Song.bulkCreate(lucySongs).then(function() {
+			process.exit();
+		});;
+  	});
+};
+
 var artistCreate = function() {
 	return DB.Artist.create({
-    name: 'Luciano Pavarotti',
-    photoUrl: 'http://img.informador.com.mx/biblioteca/imagen/677x508/811/810055.jpg',
-    nationality: 'Italiano',
-    instrument: 'Voice',
-    home_address: '1 Strada Roma'
-  });
+	    name: 'Luciano Pavarotti',
+	    photoUrl: 'http://img.informador.com.mx/biblioteca/imagen/677x508/811/810055.jpg',
+	    nationality: 'Italiano',
+	    instrument: 'Voice',
+	    home_address: '1 Strada Roma'
+  	});
 };
 
 var managerCreate = function() {
@@ -20,18 +39,28 @@ var managerCreate = function() {
 	});
 };
 
-var songCreate = function() {
-	return DB.Song.create({
-	    title: 'The Best Song Ever',
-	    duration: '3:31',
-	    date_of_release: '7/13/2015',
-	    album_title: 'Best Album Ever'
-	});
-};
+
+var lucySongs = [
+	{
+		title: "O sole mio",
+		duration: "3:21",
+		date_of_release: "1990",
+		album_title: "Three Tenors in Concert",
+		artistId: ""
+	},
+	{
+		title: "Nessun dorma",
+		duration: "3:21",
+		date_of_release: "1990",
+		album_title: "Three Tenors in Concert",
+		artistId: ""
+	}
+];
+
+
+// .then(managerCreate), create, run and catch in artist create
 
 artistCreate()
-.then(managerCreate)
-.then(songCreate)
-.then(function() {
-	process.exit();
+.then(function(artist){
+	songCreate(artist);
 });
--- starter-code/src/models/index.ts
+++ starter-code/src/models/index.ts
@@ -1,11 +1,15 @@
 //Connect
 import * as Sequelize from 'sequelize';
 
-var sequelize = new Sequelize('postgres://<username>@localhost:5432/tunr_relationships');
+var sequelize = new Sequelize('postgres://laurenwelborn@localhost:5432/tunr_relationships');
 
 var Artist = sequelize.import("./artist");
 var Manager = sequelize.import("./manager");
 var Song = sequelize.import("./song")
+
+// SQL relationships
+Song.belongsTo(Artist);
+Artist.hasMany(Song);
 
 const db = <any>{};
 db.models = {
